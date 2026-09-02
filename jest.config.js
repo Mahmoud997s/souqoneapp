292db1a2e402cb7e14b92af3aedcd78ae01ad03c
Mahmoud <mahmoud@souqone.app>
2026-09-02 13:08:23 +0300

    feat(services): add serviceWizardStore for Services Wizard Phase 1

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   preset: 'jest-expo',
-  testMatch: ['<rootDir>/**/*.spec.{ts,tsx}'],
+  testMatch: ['<rootDir>/**/*.{spec,test}.{ts,tsx}'],
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/e2e/', '<rootDir>/dist/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
